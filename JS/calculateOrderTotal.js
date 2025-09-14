99b1b610d985aa9f80e6b8b8e271cc9e05acbea0
Andrea Benz <andrealevin.benz@gmail.com>
2025-09-14 16:48:03 +0200

    calculateOrderTotal Fehler gefixt und main.js formatiert

--- JS/calculateOrderTotal.js
+++ JS/calculateOrderTotal.js
@@ -1,7 +1,8 @@
 import { currentOrder } from '../main.js';
 
 export function calculateOrderTotal() {
-    currentOrder.total = currentOrder.items.reduce((sum, item) => {
-        return (sum += item.price * item.quantity);
-    }, 0);
+    currentOrder.total = currentOrder.items.reduce(
+        (sum, item) => sum + item.price * item.quantity,
+        0,
+    );
 }
