a12cd6fcd0f8ca3b268dc0a50baa64d819007079
Andrea Benz <andrealevin.benz@gmail.com>
2025-09-16 22:38:10 +0200

    Ordner Storagedrivers erstellt

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import { emptyOrder } from './JS/emptyOrder.js';
-import { FileStorageDriver } from './JS/fileSyncSave.js';
-import { LocalStorageDriver } from './JS/localStorageSave.js';
 import { removeItem } from './JS/removeItem.js';
 import { selectItem } from './JS/selectItem.js';
 import { setPaymentMethod } from './JS/setPaymentMethod.js';
+import { FileStorageDriver } from './JS/storage-drivers/fileSyncSave.js';
+import { LocalStorageDriver } from './JS/storage-drivers/localStorageSave.js';
 
 //leere Bestellung erstellen
 export let currentOrder = emptyOrder();
